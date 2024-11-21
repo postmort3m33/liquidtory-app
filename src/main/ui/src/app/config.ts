fdb70a4f00b03d9107d11fc6416f5c932a82b3be
Joshua Touchstone <jtouch4@wgu.edu>
2024-11-21 08:28:15 -0600

    Updated Angular Config file to point to liquidtory.com, not the IP address

--- src/main/ui/src/app/config.ts
+++ src/main/ui/src/app/config.ts
@@ -4,7 +4,7 @@ const config = {
 
 // Check if running in production
 if (window.location.hostname !== 'localhost') {
-  config.baseUrl = 'http://44.196.216.74';      // HTTPS for API
+  config.baseUrl = 'http://liquidtory.com';      // HTTPS for API
 }
 
 export default config;
